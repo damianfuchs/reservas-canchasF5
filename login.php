5dc6807417764d96441da0f47602eb7f9980ab84
damianfuchs <damianfuchs_1999@hotmail.com>
2024-01-25 16:49:28 -0300

    Creacion de Login

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fulbito</title>
+</head>
+<body>
+    <div class="cuadro-login">
+        <form method="post">
+            <div class="dni">
+                    <label for="">DNI</label>
+                    <input type="text" require>
+            </div>
+            <div class="password">
+                    <label for="">Contraseña</label>
+                    <input type="text" require>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
