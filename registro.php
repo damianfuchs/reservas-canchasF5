615c45ddb396d2333b79c2b4962c8c9e689ff045
damianfuchs <damianfuchs_1999@hotmail.com>
2024-01-25 18:34:03 -0300

    Diseño de login y registro

--- registro.php
+++ registro.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/login.css">
-    <title>FulbitoF5 Ingresar</title>
+    <title>FulbitoF5 Registrarse</title>
 </head>
 <body>
     <div class="login-box">
@@ -16,23 +16,26 @@
             <header>FULBITO F5</header>
         </div>
         <div class="input-box">
+            <input type="text" class="input-field" placeholder="Nombre Completo" autocomplete="off" required>
+        </div>
+        <div class="input-box">
             <input type="text" class="input-field" placeholder="DNI" autocomplete="off" required>
+        </div>
+        <div class="input-box">
+            <input type="text" class="input-field" placeholder="Numero de Telefono" autocomplete="off" required>
         </div>
         <div class="input-box">
             <input type="password" class="input-field" placeholder="Contraseña" autocomplete="off" required>
         </div>
+        <div class="input-box">
+            <input type="password" class="input-field" placeholder="Confirmar Contraseña" autocomplete="off" required>
+        </div>
         <div class="forgot">
 
-            <section>
-                <a href="#">Olvidaste la Contraseña?</a>
-            </section>
         </div>
         <div class="input-submit">
             <button class="submit-btn" id="submit"></button>
-            <label for="submit">Entrar</label>
-        </div>
-        <div class="sign-up-link">
-            <p>No tenes cuenta? <a href="#">Registrarse</a></p>
+            <label for="submit">Registrarse</label>
         </div>
     </div>
 </body>
