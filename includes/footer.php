bf1911543406802af395058292f7e3c7005fe100
damianfuchs <damianfuchs_1999@hotmail.com>
2024-01-29 22:43:39 -0300

    Diseño del Footer

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/footer.css">
+    <script src="https://kit.fontawesome.com/69af035f89.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="footerContainer">
+        <div class="socialIcon">
+            <a href=""><i class="fa-brands fa-square-instagram fa-xl" style="color: #ffffff;"></i></a>
+            <a href=""><i class="fa-brands fa-square-facebook fa-xl" style="color: #ffffff;"></i></a>
+            <a href=""><i class="fa-brands fa-square-whatsapp fa-xl" style="color: #ffffff;"></i></a>
+        </div>
+        <div class="footerNav">
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Reservar Cancha</a></li>
+                <li><a href="">Perfil</a></li>
+                <li><a href="">Informacion</a></li>
+            </ul>
+        </div>
+        <div class="footerBottom">
+            <p>Copyright &copy;2024; Desarrolado por <span class="Desarrollador">Damian Emmanuel Fuchs</span></p>
+        </div>
+    </div>
+</body>
+</html>
